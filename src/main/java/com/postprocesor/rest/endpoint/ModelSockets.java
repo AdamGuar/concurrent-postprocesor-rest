0096fe459aa134e49b91f381f5dd3f36b562031f
karolskora1993 <karol-skora@poczta.pl>
2018-06-06 09:36:40 +0200

    configure websockets

--- src/main/java/com/postprocesor/rest/endpoint/ModelSockets.java
+++ src/main/java/com/postprocesor/rest/endpoint/ModelSockets.java
@@ -1,4 +1,34 @@
 package com.postprocesor.rest.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.util.HtmlUtils;
+
+import com.postprocesor.rest.model.Model;
+import com.postprocesor.rest.service.ModelService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Controller
 public class ModelSockets {
+
+    ModelService service;
+
+    @Autowired
+    public ModelSockets(ModelService service) {
+        this.service = service;
+    }
+
+    @MessageMapping("/model")
+    @SendTo("/model")
+    public Mono<Model> sendModel(@PathVariable String modelname) throws Exception {
+
+        return service.getModelById(modelname);
+    }
+
+
 }
